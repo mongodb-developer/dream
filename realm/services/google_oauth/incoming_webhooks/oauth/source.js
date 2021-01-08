810b6bac6942d102032c25127316964cee9e73f6
Mark Smith <judy@judy.co.uk>
2021-01-08 12:29:38 +0000

    Updating webhooks and cleaning up some files.

--- realm/services/google_oauth/incoming_webhooks/oauth/source.js
+++ realm/services/google_oauth/incoming_webhooks/oauth/source.js
@@ -1,17 +1,17 @@
 
-exports = async function (payload, response) {
+exports = async function(payload, response) {
   const https = require('https');
   const querystring = require('querystring');
   const url = require('url');
-
+  
   // MongoDBofficial Channel ID:
-  // (Obtained from: https://developers.google.com/youtube/v3/docs/channels/list?apix=true&apix_params=%7B%22part%22%3A%5B%22id%22%5D%2C%22forUsername%22%3A%22MongoDB%22%7D)
+  // (Obtained from: https://www.youtube.com/account_advanced)
   const ACCOUNT_ID = context.values.get("GOOGLE_ACCOUNT_ID"); //"UCK_m2976Yvbx-TyDLw7n1WA";
-
+  
   // Following obtained from: https://console.developers.google.com/apis/credentials
   const CLIENT_ID = context.values.get("GOOGLE_CLIENT_ID");
   const CLIENT_SECRET = context.values.get("GOOGLE_CLIENT_SECRET");
-
+  
   // TODO: Can probably generate the following programmatically:
   const OAUTH2_CALLBACK = context.request.webhookUrl;
 
@@ -32,9 +32,9 @@ exports = async function (payload, response) {
     "https://www.googleapis.com/auth/youtubepartner",
     "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
     "https://www.googleapis.com/auth/yt-analytics.readonly"
-  ];
+    ];
 
-
+  
   const error = payload.query.error;
   if (typeof error !== 'undefined') {
     // Google says there's a problem:
@@ -51,8 +51,11 @@ exports = async function (payload, response) {
         'redirect_uri': OAUTH2_CALLBACK,
         'response_type': 'code',
         'scope': SCOPES.join(' '),
+        'access_type': "offline",
       });
-
+      
+      console.log("Redirecting to:", oauthURL.href.substring(oauthURL.href.length - 100));
+  
       response.setStatusCode(302);
       response.setHeader('Location', oauthURL.href);
     } else {
@@ -66,31 +69,30 @@ exports = async function (payload, response) {
           code: oauthCode,
           grant_type: 'authorization_code',
           redirect_uri: OAUTH2_CALLBACK,
-          access_type: "offline",
         },
         encodeBodyAsJSON: true,
       });
+      
+      // TODO: Need to handle errors here!
+      
       let tokens = JSON.parse(res.body.text());
-      tokens._id = "youtube";
-
-      const doc = await context.services.get("mongodb-atlas").db("auth").collection("auth_tokens").findOneAndReplace(
+      tokens.updated = new Date();
+      tokens.expires_at = new Date();
+      tokens.expires_at.setTime(Date.now() + (tokens.expires_in * 1000));
+      
+      await context.services.get("mongodb-atlas").db("auth").collection("auth_tokens").findOneAndReplace(
         {
           _id: "youtube"
-        },
+        }, 
         tokens,
         {
           upsert: true,
         },
       );
-
+      
       return {
         "message": "ok",
       }
     }
   }
-
-  // Querying a mongodb service:
-  // const doc = context.services.get("mongodb-atlas").db("dbname").collection("coll_name").findOne();
-
-
 };
---
+++ realm/services/google_oauth/incoming_webhooks/test/source.js
@@ -0,0 +1,16 @@
+exports = async function(payload, response) {
+    // Get a token (it'll be refreshed if necessary):
+    const accessToken = await context.functions.execute("get_token");
+    
+    // Make an authenticated call:
+    const result = await context.http.get({
+      url: 'https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=MongoDB&prettyPrint=true',
+      headers: {
+        'Authorization': [`Bearer ${accessToken}`],
+        'Accept': ['application/json'],
+      },
+    });
+    
+    response.setHeader('Content-Type', 'text/plain');
+    response.setBody(result.body.text());
+};
