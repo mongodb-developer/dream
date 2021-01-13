2b67d724bcb39d8754da0911d5e75a5567e81a14
Lauren Schaefer <lauren.schaefer@mongodb.com>
2021-01-13 14:41:36 -0500

    Loops are hard

--- realm/functions/get_previous_youtube_video_stats/source.js
+++ realm/functions/get_previous_youtube_video_stats/source.js
@@ -1,4 +1,5 @@
 exports = async function (year, month, firstDay, firstDay) {
+  // NOTE: We recommend gathering up to 15 days of stats to prevent timeouts
     
   for (let day = firstDay; day <= firstDay; day++) {
     await context.functions.execute("get_youtube_video_stats", year, month, day);
