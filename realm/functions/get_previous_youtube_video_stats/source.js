00c42339b769d9df81dcb43d4b97915c82d27761
Lauren Schaefer <lauren.schaefer@mongodb.com>
2021-01-13 14:42:00 -0500

    Duh

--- realm/functions/get_previous_youtube_video_stats/source.js
+++ realm/functions/get_previous_youtube_video_stats/source.js
@@ -1,7 +1,7 @@
 exports = async function (year, month, firstDay, firstDay) {
   // NOTE: We recommend gathering up to 15 days of stats to prevent timeouts
     
-  for (let day = firstDay; day <= firstDay; day++) {
+  for (let day = firstDay; day <= lastDay; day++) {
     await context.functions.execute("get_youtube_video_stats", year, month, day);
     console.log(`YouTube video stats imported for ${year}-${month}-${day}`);    
   }  
