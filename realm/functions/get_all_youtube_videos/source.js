1bd2eb3ac6143df42aa39c7319edc1fa0843347c
Lauren Schaefer <lauren.schaefer@mongodb.com>
2021-01-14 12:46:47 -0500

    Clean function

--- realm/functions/get_all_youtube_videos/source.js
+++ realm/functions/get_all_youtube_videos/source.js
@@ -1,55 +1,42 @@
 exports = async function () {
-  // Get a token (it'll be refreshed if necessary):
-  const accessToken = await context.functions.execute("get_token");
   
-  let nextPageToken;
-      
-  // Duplicating code here so I can get the initial nextPageToken -- need to clean this up
-  
-  // Let's make an actual API request:
-  let videoResults = await context.http.get({
-    // hard coding the MongoDB uploads playlist ID
-    url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUK_m2976Yvbx-TyDLw7n1WA',
-    headers: {
-      'Authorization': [`Bearer ${accessToken}`],
-      'Accept': ['application/json'],
-    },
-  });
-  
-  const ejson_body = EJSON.parse(videoResults.body.text());
-  
-  nextPageToken = ejson_body.nextPageToken;
-  console.log(nextPageToken);
-  
-
-  ejson_body.items.forEach( function(video)  {
-    video._id = video.snippet.resourceId.videoId;
-    video.snippet.publishedAt = new Date(video.snippet.publishedAt)
-    const doc = context.services.get("mongodb-atlas").db("dream").collection("youtube_videos").updateOne({ "_id": `${video._id}` }, {$set: video}, { "upsert": true });
-  });
+  // Setting nextPageToken to true initially to get us inside the "while" loop.
+  // nextPageToken will be set to the nextPage token that the YouTube API provides
+  // after we get initial results
+  let nextPageToken = true;
   
   while(nextPageToken){
     
-    // Let's make an actual API request:
+    // Get a token (it'll be refreshed if necessary):
+    const accessToken = await context.functions.execute("get_token");
+
+    // The first time we enter this while loop, we won't have a nextPageToken from YouTube
+    let nextPageTokenParam = "";
+    if (nextPageToken !== true) {
+      nextPageTokenParam = `&pageToken=${nextPageToken}`;
+    }
+    
     let videoResults = await context.http.get({
-      // hard coding the MongoDB uploads playlist ID
-      url: `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUK_m2976Yvbx-TyDLw7n1WA&pageToken=${nextPageToken}`,
+      // hard coding the MongoDB uploads playlist ID for now
+      // This id can be generated using the YouTube API when we want to make this generic
+      url: `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUK_m2976Yvbx-TyDLw7n1WA${nextPageTokenParam}`,
       headers: {
         'Authorization': [`Bearer ${accessToken}`],
         'Accept': ['application/json'],
       },
     });
     
-    const ejson_body = EJSON.parse(videoResults.body.text());
-    
+    const ejson_body = EJSON.parse(videoResults.body.text());    
     nextPageToken = ejson_body.nextPageToken;
-    console.log(nextPageToken);
-    
 
+    if (!ejson_body.items) {
+      throw "No videos returned";
+    }
     ejson_body.items.forEach( function(video)  {
       video._id = video.snippet.resourceId.videoId;
       video.snippet.publishedAt = new Date(video.snippet.publishedAt)
-      const doc = context.services.get("mongodb-atlas").db("dream").collection("youtube_videos").updateOne({ "_id": `${video._id}` }, {$set: video}, { "upsert": true });
+      const doc = context.services.get("mongodb-atlas").db("dream").collection("youtube_videos").updateOne({ "_id": `${video._id}` }, { $set: video }, { "upsert": true });
+      console.log(doc);
     });
   }
 
