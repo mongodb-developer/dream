d763249c83edad54992d2bab789c536e093b07dd
Lauren Schaefer <lauren.schaefer@mongodb.com>
2021-01-14 13:19:09 -0500

    More cleaning

--- realm/functions/get_youtube_video_stats/source.js
+++ realm/functions/get_youtube_video_stats/source.js
@@ -12,34 +12,27 @@ exports = async function (year, month, day) {
 
   let date = `${year}-${month}-${day}`;
   
-  let videos = '';
-  const docs = await context.services.get("mongodb-atlas").db("dream").collection("youtube_videos").find({}).toArray();
-  const docsLength = docs.length;
+  const allYouTubeVideos = await context.services.get("mongodb-atlas").db("dream").collection("youtube_videos").find({}).toArray();
+  const totalNumberOfYouTubeVideos = allYouTubeVideos.length;
   
+  // We will paginate the videos in groups of 500
   let pageIncrement = 0;
   let numberOfDocsPerPage = 500;
-  while (pageIncrement < docsLength) {
-    console.log(`pageIncrement: ${pageIncrement}`);
-    videos = "";
-    
-    for (i = pageIncrement; i < pageIncrement + numberOfDocsPerPage; i++){
-      console.log(`i: ${i}, pageIncrement: ${pageIncrement}, numberOfDocsPerPage: ${numberOfDocsPerPage}`);
-      if (i >= (docsLength - 1) ) break;
-      videos += `${docs[i]._id},`;
-      console.log(`${docs[i]._id},`);
-    }
-    console.log(videos.slice(-30));
-    console.log(`number of Videos ${ (videos.length / 12)}`);
+  
+  while (pageIncrement < totalNumberOfYouTubeVideos) {
 
-    //remove final trailing comma
-    videos = videos.slice(0, -1);
-    console.log(videos);
-    console.log(docs.length);      
+    // Generate a list of videos for this page
+    let videos = "";
+    for (i = pageIncrement; i < pageIncrement + numberOfDocsPerPage; i++){
+      if (i >= (totalNumberOfYouTubeVideos - 1) ) break;
+      videos += `${allYouTubeVideos[i]._id},`;
+    }
+    // Remove final trailing comma from the videos list
+    videos = videos.slice(0, -1);   
       
     // Get a token (it'll be refreshed if necessary):
     const accessToken = await context.functions.execute("get_token");
     
-    // hard coding the MongoDB uploads playlist ID
     const url = `https://youtubeanalytics.googleapis.com/v2/reports?dimensions=video&endDate=${date}&ids=channel%3D%3DMINE&metrics=views%2Clikes%2Cdislikes%2Cshares%2Ccomments%2CestimatedMinutesWatched%2CaverageViewDuration%2CaverageViewPercentage%2CsubscribersGained%2CsubscribersLost%2CvideosAddedToPlaylists%2CvideosRemovedFromPlaylists&sort=-views&startDate=${date}&filters=video==${videos}`;
     
     let statsResults = await context.http.get({
@@ -52,45 +45,42 @@ exports = async function (year, month, day) {
     
     const ejson_body = EJSON.parse(statsResults.body.text());
     
-    if(ejson_body.rows) {        
-      ejson_body.rows.forEach( function(video)  {
-        const videoId = video[0];
-        const update = {
-          "$set": {
-            videoId,
-            year,
-            month: parseInt(month)
-          },
-          "$addToSet": {
-            "stats": {
-              "date": new Date(date),
-              "views": video[1],
-              "likes": video[2],
-              "dislikes": video[3],
-              "shares": video[4],
-              "comments": video[5],
-              "estimatedMinutesWatched": video[6],
-              "averageViewDuration": video[7],
-              "averageViewPercentage": video[8],
-              "subscribersGained": video[9],
-              "subscribersLost": video[10],
-              "videosAddedToPlaylists": video[11],
-              "videosRemovedFromPlaylists": video[12]
-            }
-          }
-        };
-        //Todo: probably want to copy data like the name, isDA, and creator names here
-        //note: could do some calculations here and store them in the docs
-        const options = { "upsert": true };
-        
-        const doc =  context.services.get("mongodb-atlas").db("dream").collection("youtube_stats").updateOne({ "_id": `${videoId}_${year}_${month}` }, update, options);
-      });
+    if (!ejson_body.rows) {
+      throw `No stats returned. ${ejson_body.error.code}: ${ejson_body.error.message}`;;
     }
+      
+    ejson_body.rows.forEach( function(video)  {
+      const videoId = video[0];
+      const update = {
+        "$set": {
+          videoId,
+          year,
+          month: parseInt(month)
+        },
+        "$addToSet": {
+          "stats": {
+            "date": new Date(date),
+            "views": video[1],
+            "likes": video[2],
+            "dislikes": video[3],
+            "shares": video[4],
+            "comments": video[5],
+            "estimatedMinutesWatched": video[6],
+            "averageViewDuration": video[7],
+            "averageViewPercentage": video[8],
+            "subscribersGained": video[9],
+            "subscribersLost": video[10],
+            "videosAddedToPlaylists": video[11],
+            "videosRemovedFromPlaylists": video[12]
+          }
+        }
+      };
+
+      const options = { "upsert": true };
+      
+      context.services.get("mongodb-atlas").db("dream").collection("youtube_stats").updateOne({ "_id": `${videoId}_${year}_${month}` }, update, options);
+    });
     
     pageIncrement += numberOfDocsPerPage;
   }
-
-  return "Data imported";
 };
-
-
